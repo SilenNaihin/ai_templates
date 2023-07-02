ced6aac2b6cf7898ce86157174c8764c090e1c06
Silen Naihin <silen.naihin@gmail.com>
2023-07-01 23:00:09 -0400

    gitignore and fix chatpair import

--- aitemplates/__init__.py
+++ aitemplates/__init__.py
@@ -5,5 +5,5 @@ from .oai.responses.embedding import get_embedding
 from .oai.ApiManager import ApiManager, SingleApiManager
 
 from .oai.types.base import Message, ResponseDict
-from .oai.types.chat import ChatSequence, ChatConversation
+from .oai.types.chat import ChatSequence, ChatConversation, ChatPair
 from .oai.types.functions import FunctionPair, FunctionsAvailable
